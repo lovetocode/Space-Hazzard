079560f1ce9aa6955c24cb0e2c1060b8700f72c9
Daniel Thornton <dshaggy28@yahoo.com>
2023-09-09 14:47:20 -0700

    added game over screen

--- Scene/Scripts/game.gd
+++ Scene/Scripts/game.gd
@@ -9,23 +9,35 @@ extends Node2D
 @onready var enemy_container = $EnemyContainer
 @onready var Enemy_spawnTimer = $EnemySpawnTimer
 @onready var hud = $UILayer/HUD
+@onready var gos = $UILayer/GameOver
 
 var player = null
 var score := 0:
 	set(value):
 		score = value
 		hud.score = score
+var high_score
 
 func _ready():
+	var save_file = FileAccess.open("user://sava.data", FileAccess.READ)
+	if save_file != null:
+		high_score = save_file.get_32()
+	else:
+		high_score = 0 
+		save_game()
 	score = 0
 	player = get_tree().get_first_node_in_group("Player")
 	assert(player!=null)
 	player.global_position = playerSpawnPosition.global_position
 	player.laser_shot.connect(on_player_laser_shot)
+	player.killed.connect(on_player_killed)
+	
+func save_game():
+	var save_file = FileAccess.open("user://sava.data", FileAccess.WRITE)
+	save_file.store_32(high_score)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+# Called every frame. 'delta' is the elapsed time since the previous frame.func _process(delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("reset"):
@@ -44,5 +56,13 @@ func _on_enemy_spawn_timer_timeout():
 	
 func on_enemy_killed(points):
 	score += points
+	if score > high_score:
+		high_score = score
 	
+func on_player_killed():
+	gos.set_score(score) 
+	gos.set_high_Score(high_score)
+	save_game()
+	await get_tree().create_timer(1.5).timeout
+	gos.visible = true
 	
--- Scene/Scripts/gameOver.gd
+++ Scene/Scripts/gameOver.gd
@@ -3,8 +3,8 @@ extends Control
 func set_score(value):
 	$Panel/Score.text = "Score: " + str(value)
 	
-func set_HighScore(value):
-	$Panel/HighScore.text = "High Score: " + str(value)
+func set_high_Score(value):
+	$Panel/highScore.text = "High-Score: " + str(value)
 
 func _on_button_pressed():
 	get_tree().reload_current_scene()
